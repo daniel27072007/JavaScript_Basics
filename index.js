5c3914ea33713ab16302ddae575628476fcff38e
Daniel Belculfine <44295798+daniel27072007@users.noreply.github.com>
2026-04-19 15:17:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+//About Varibles
+//var are the varibles that can change the most, then comes the let varibles and the const are those varibles that you dont want to change like the button of the page for exemplelet xp = 0;
+let hp = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+//instead of everytime you need to reffer about button1 you write "document.querySelector("#button1");" with the const button1 you just need to write "button1"!
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("goldText");
+const monsterStats = document.querySelector("monsterStats");
+const monsterNameText = document.querySelector("monsterNameText");
+const monsterHealthText = document.querySelector("monsterHealthText");
+
+
+
