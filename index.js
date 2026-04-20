ea5400444e93f73a106d8d77ea75452908c7e2b7
Daniel Belculfine <44295798+daniel27072007@users.noreply.github.com>
2026-04-20 14:47:53 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -19,5 +19,56 @@ const monsterStats = document.querySelector("monsterStats");
 const monsterNameText = document.querySelector("monsterNameText");
 const monsterHealthText = document.querySelector("monsterHealthText");
 
+const locations = [
+    {
+        name: "town square",
+        "button text": ["Go to store", "Go to cave", "Fight dragon"],
+        "button functions": [goStore, goCave, fightDragon],
+        text: "You enter the Town Square. You see a sign thats says \"Store.\""
+    },
+    {
+        name: "store",
+        "button text": ["Buy 10 Health (10 Gold)", "Buy Weapon (30 Gold)", "Go to Town Square"],
+        "button functions": [buyHealth, buyWeapon, goTown],
+        text: "You enter the store."
+    }
+]
 
+//inicialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function update (locations){
+    button1.innerText = locations["button text"][0];
+    button2.innerText = locations["button text"][1];
+    button3.innerText = locations["button text"][2];
+    button1.onclick = locations["button functions"][0];
+    button2.onclick = locations["button functions"][1];
+    button3.onclick = locations["button functions"][2];
+    text.innerText = locations.text;
+}
+
+function goTown(){
+    update(locations[0]);
+}
+
+function goStore(){
+    update(locations[1]);
+}
+
+function buyHealth(){
+
+}
+
+function buyWeapon(){
+
+}
+
+function goCave(){
+    console.log("Going to cave.");
+}
+function fightDragon(){
+    console.log("Going fight the dragon.");
+}
 
